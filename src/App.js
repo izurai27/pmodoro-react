a9536591ccf3b85924d10e999b2022e709a2e085
Izma Raifani <izurai@gmail.com>
2022-03-12 23:27:06 +0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+
+function Length (props){
+  if(props.value>60 ){
+    return <div id={props.id}>60</div>
+  } else if (props.value<1){
+    return <div id={props.id}>1</div>
+  }
+  return <div id={props.id}>{props.value}</div>
+}
+
+
+
+
+
+
 
 function App() {
+  const defaultSessionLength = 25;
+  const defaultBreakLength = 5;
+
+  const [sessionLength, setSessionLength] = useState(defaultSessionLength);
+  const [breakLength, setBreakLength] = useState(defaultBreakLength);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <title>Pomodoro App</title>
+
+      <div id="timer-label">session</div>
+      <div id="time-left">00:00</div>
+      <div id="start_stop">start</div>
+      <div id="reset" 
+            onClick={() => {
+              setBreakLength(defaultBreakLength)
+              setSessionLength(defaultSessionLength)
+              }}>reset</div>
+     
+      <div className="editSessionBreak">
+        <div className='breakGroup'>
+          <div id="break-label">Break Length</div>
+          <Length id="break-length" value={breakLength}/>
+          <div id="break-increment" onClick={() => setBreakLength(breakLength + 1)}>+</div>
+          <div id="break-decrement" onClick={() => setBreakLength(breakLength - 1)}>-</div>
+        </div>
+
+        <div className='sessionGroup'>
+          <div id="session-label">Session Length</div> 
+          <Length id="session-length" value={sessionLength}/>
+          <div id="session-increment" onClick={() => setSessionLength(sessionLength + 1)}>+</div>
+          <div id="session-decrement" onClick={() => setSessionLength(sessionLength - 1)}>-</div>
+        </div>
+
+      </div>
+            
+      <div></div>
+      <div></div>
     </div>
   );
 }
 
 export default App;
+
